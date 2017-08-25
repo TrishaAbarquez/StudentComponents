8a8b75af0c426f1bf40dfafabb86962f96b99ecb
TrishaAbarquez <31053873+TrishaAbarquez@users.noreply.github.com>
2017-08-25 10:46:42 +0800

    Initial commit

---
+++ src/app/input-students/input-students.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'input-students',
+  templateUrl: './input-students.component.html',
+  styleUrls: ['./input-students.component.css']
+})
+export class InputStudentsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
