5a1f4dc72f9f398b5288f3618f36cec615620855
TrishaAbarquez <31053873+TrishaAbarquez@users.noreply.github.com>
2017-08-30 10:53:08 +0800

    update 3.0

--- src/app/input-students/input-students.component.ts
+++ src/app/input-students/input-students.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, Input, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'input-students',
@@ -6,12 +6,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./input-students.component.css']
 })
 export class InputStudentsComponent implements OnInit {
+  
+  studentCollection: Array<object> = [];
    studNo: number;
   studFname: string;
   studLname: string;
   studProg: string;
   studYr: number;
   studentRecord: object;
+    private checkPatterns(value:any , pattern: RegExp): boolean {
+    if(pattern.test(value))
+      return true;
+    else 
+      return false;
+  }
+    @Output() AddStudents = new EventEmitter<any>();
+  @Input() message;
+
+  flag = 'addStudents';
+  messages = '';
+  printing = false;
   addStudentEntry(){
     this.studentRecord = {
           studNumber: this.studNo,
@@ -20,9 +34,41 @@ export class InputStudentsComponent implements OnInit {
           studProgram: this.studProg,
           studYear: this.studYr
         };
-return this.studentRecord;
 
-}
+this.AddStudents.emit ({mode: this.flag});
+    this.printing = false;
+    const stringPattern = /^[A-z\s]+$/;
+    const studNumberPattern = /^[0-9]+$/;
+    const studYearPattern = /^[1-5]+$/;
+
+    if (this.checkPatterns(this.studNo, studNumberPattern) &&
+      this.checkPatterns(this.studFname, stringPattern) &&
+      this.checkPatterns(this.studLname, stringPattern) &&
+      this.checkPatterns(this.studProg, stringPattern) &&
+      this.checkPatterns(this.studYr, studYearPattern)) {
+
+        this.studentRecord = {
+          studNumber: this.studNo,
+          studFirstName: this.studFname,
+          studLastName: this.studLname,
+          studProgram: this.studProg,
+          studYear: this.studYr
+        };
+        this.studentCollection.push(this.studentRecord);
+        this.messages = null;
+        this.clearValues();
+    } else {
+      this.messages = 'Errors have been encountered and therefore cannot continue to process requested operation.';
+      return false;
+    }
+  }
+  clearValues(): void{
+    this.studNo = null;
+    this.studFname = null;
+    this.studLname= null;
+    this.studProg = null;
+    this.studYr = null;
+  }
   constructor() { }
 
   ngOnInit() {
